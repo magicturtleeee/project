9ed78072dbd1b549cdbc2c472ba85e7f9a102475
magicturtleeee <105358082+magicturtleeee@users.noreply.github.com>
2022-06-11 23:25:18 +0300

    Update vsn.py

--- vsn.py
+++ vsn.py
@@ -58,5 +58,5 @@ gdften=gdf1.sjoin(gdfnew,predicate="intersects",how='inner')
 gdften
 num=gdften['index_right'].value_counts()
 d=gdf1.merge(num,left_index=True,right_index=True)
-figure=d.plot(column='index_right', cmap='PuRd', legend=True)
-st.pyplot(figure)
+d.plot(column='index_right', cmap='PuRd', legend=True)
+
