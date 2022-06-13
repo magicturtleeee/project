d7ba9a7876f1510454b9f19646a632d8d89caf7a
magicturtleeee <105358082+magicturtleeee@users.noreply.github.com>
2022-06-13 10:37:54 +0300

    Update tennis data.py

--- tennis data.py
+++ tennis data.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import requests
 import re
+import sqlite3
 
 # используя селениум, получаем данные про топ теннисисток из wta
 driver = webdriver.Chrome('/Users/annagushchina/Downloads/chromedriver')
@@ -69,3 +70,29 @@ for i in range (len(r['features'])):
     phone.append(r['features'][i]['properties']['Attributes']['HelpPhone_en'])
 dfcourts=pd.DataFrame({'name':name,'address':ad, 'email':mail, 'website':web, 'phone': phone, 'lat':lat, 'lon':lon})
 dfcourts.to_csv('courts.csv', index=False)
+
+#сейчас используем SQL для обработки данных с турниров. Сначала загрузим данные в базу.
+df10=pd.read_csv('wta_matches_2022.csv')
+conn=sqlite3.connect("database.sqlite")
+c=conn.cursor()
+df10.to_sql("wta", conn) #сейчас наша таблица есть в базе данных
+c.execute(
+"""
+SELECT match_num FROM wta
+WHERE tourney_name='Australian Open'
+""").fetchall().   #узнавли, что номера матчей Australian open с 100 по 226 в таблице, найдем победительницу
+win=c.execute(
+"""
+SELECT winner_name FROM wta
+WHERE tourney_name='Australian Open' and match_num='226'
+""").fetchall() # нашли победительницу 'Ashleigh Barty'
+num=c.execute(
+"""
+SELECT COUNT (DISTINCT loser_name) AS number FROM wta 
+WHERE tourney_name='Australian Open'
+""").fetchall() #мы нашли, что проигравших 127, значит участников 127+победительница=128. Будем строить граф с матчами.
+df11=pd.read_sql("""
+SELECT * FROM wta 
+WHERE tourney_name='Australian Open'
+""", conn)
+df11.to_csv("aus2022.csv") #нашли все данные про нужный нам турнир и сохранили в csv
